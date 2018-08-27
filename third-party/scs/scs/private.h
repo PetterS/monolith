dc0e019055cb329ac414d5e00ef89627f795f0ea
Petter Strandmark <petter.strandmark@gmail.com>
2018-08-27 19:27:10 +0200

    Add SCS to third-party. https://github.com/cvxgrp/scs

---
+++ third-party/scs/scs/private.h
@@ -0,0 +1,20 @@
+#ifndef PRIV_H_GUARD
+#define PRIV_H_GUARD
+
+#include "glbopts.h"
+#include "scs.h"
+#include "cs.h"
+#include "amd.h"
+#include "ldl.h"
+#include "common.h"
+
+struct PRIVATE_DATA {
+    cs *L;         /* KKT, and factorization matrix L resp. */
+    scs_float *D;  /* diagonal matrix of factorization */
+    scs_int *P;    /* permutation of KKT matrix for factorization */
+    scs_float *bp; /* workspace memory for solves */
+    /* reporting */
+    scs_float totalSolveTime;
+};
+
+#endif
