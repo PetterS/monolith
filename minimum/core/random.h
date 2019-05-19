3bc2e0125fe1312baf2a0940a54d255c81d651dd
Petter Strandmark <petter.strandmark@gmail.com>
2019-05-19 13:07:23 +0200

    Fix random.h

--- minimum/core/random.h
+++ minimum/core/random.h
@@ -16,14 +16,14 @@ Engine seeded_engine() {
 	std::generate(std::begin(seed_data), std::end(seed_data), std::ref(source));
 	std::seed_seq seeds(std::begin(seed_data), std::end(seed_data));
 	Engine engine(seeds);
-	engine;
+	return engine;
 }
 
 template <class Engine, typename... Ts>
 Engine repeatably_seeded_engine(Ts... is) {
 	std::seed_seq seeds = {is...};
 	Engine engine(seeds);
-	engine;
+	return engine;
 }
 }  // namespace core
 }  // namespace minimum
