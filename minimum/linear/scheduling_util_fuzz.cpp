2f38192a87abbee792222578e48762f38403a737
Petter Strandmark <petter.strandmark@gmail.com>
2020-05-12 23:17:36 +0200

    Fix compilation error in scheduling_util_fuzz.

--- minimum/linear/scheduling_util_fuzz.cpp
+++ minimum/linear/scheduling_util_fuzz.cpp
@@ -12,7 +12,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* raw_data, size_t size) {
 	string data((char*)raw_data, size);
 	istringstream sin(data);
 	try {
-		auto problem = read_nottingham_instance(sin, false);
+		auto problem = ::minimum::linear::read_nottingham_instance(sin, false);
 		// cout << "Number of vars: " << ip->get_number_of_variables() << "\n";
 	} catch (std::logic_error&) {
 		// From minimum_core_assert
